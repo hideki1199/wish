c0dfe0dddf233f301109c83378ebcdded5be4aec
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-04-21 23:37:12 +0900

    Add create function description in profiles-controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,15 +3,23 @@ class ProfilesController < ApplicationController
   def index
     @information = Profile.find(1)
     @search = Profile.where('name LIKE(?)', "%#{params[:keyword]}%")
-
   end
 
   def new
+    @profile = Profile.new
+  end
 
+  def create
+    Profile.create(create_params)
+    redirect_to root_path
   end
 
   def show
 
   end
 
+  private
+  def create_params
+    params.permit(:name, :current_job, :past_job, :business_skills, :private_slills, :business_connection, :private_connection, :qualification)
+  end
 end
--- db/migrate/20170315063003_create_profiles.rb
+++ db/migrate/20170315063003_create_profiles.rb
@@ -4,12 +4,11 @@ class CreateProfiles < ActiveRecord::Migration
       t.string  :name,            null: false
       t.text  :current_job,        null: false
       t.text  :past_job,           null: false
-      t.text  :youcan_job,         null: false
-      t.text  :youcan_notjoc,      null: false
-      t.text  :connection_notjob,  null: false
-      t.text  :connection_notjob,  null: false
+      t.text  :business_skills,     null: false
+      t.text  :private_skills,     null: false
+      t.text  :business_connection,  null: false
+      t.text  :private_connection,   null: false
       t.text  :qualification,      null: false
-    # ここに全カラム書いて、rake db:migrationをターミナルでうつと、テータベースが作成される。
 
       t.timestamps null: false
     end
